4f9ac722d3fa877fadbf658e7b512195fe60a00d
LeoSallu <L.sallustio99@gmail.com>
2023-03-01 16:09:49 +0100

    funzione completata passaggio a if

--- Palindromo/js/main.js
+++ Palindromo/js/main.js
@@ -1,2 +1,21 @@
 'use strict';
+// Console input utente 
+const userInput= prompt('Inserisci una parola');
+console.log(userInput);
+// Console Word Palindroma
+const wordPal= wordReverse(userInput);
+console.log(wordPal);
 
+
+
+
+
+
+// FUNZIONE PAROLA ROVESCIATA 
+function wordReverse(word){
+    let reverseWord = '';
+    for (let i=word.length-1; i>=0; i--){
+        reverseWord += word[i];
+    }
+    return reverseWord;
+}
